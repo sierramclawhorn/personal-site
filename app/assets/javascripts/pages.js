a985c0537b62d22861779ef24fe6cc553939c79d
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-26 10:52:44 -0800

    Add descriptions to tagline

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,12 @@
-setInterval(function() {
-    $('.taglineTxt').fadeOut(200, function() {
-        var $this = $(this);
-        $this.text($this.text() == 'human typewriter' ? 'potato enthusiast' : 'human typewriter');        
-        $this.toggleClass('textAnimatedOne textAnimatedTwo');        
-        $this.fadeIn(200);
-    });
-}, 3000);
+$(function() {
+    counter = 0;
+    txtArray = ['human typewriter', 'potato enthusiast', 'creature appellomancer', 'salad propagator', 'benjamin franklin believer'];
+    setInterval(function() {
+    	counter++;
+    	$(".taglineTxt").fadeOut(200, function() {
+        	$(this)
+        		.text(txtArray[counter % txtArray.length])
+        		.fadeIn(200);
+    	});
+    }, 3000);
+});
