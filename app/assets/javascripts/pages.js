0d2d976e7dc242186bebc2012b648e9897402ea9
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-26 15:50:07 -0800

    Add image icons; jQuery slide for nav bar

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,12 +1,23 @@
+$(document).ready(function(){
+
+    $('.nameHeader').on('click', function() {
+        $(this).parent().parent().find('.navLinks').slideToggle();
+    })
+
+});
+
+
+
+
 $(function() {
     counter = 0;
     txtArray = ['human typewriter', 'potato enthusiast', 'creature appellomancer', 'salad propagator', 'benjamin franklin believer'];
     setInterval(function() {
-    	counter++;
-    	$(".taglineTxt").fadeOut(200, function() {
-        	$(this)
-        		.text(txtArray[counter % txtArray.length])
-        		.fadeIn(200);
-    	});
+        counter++;
+        $(".taglineTxt").fadeOut(200, function() {
+            $(this)
+                .text(txtArray[counter % txtArray.length])
+                .fadeIn(200);
+        });
     }, 3000);
 });
