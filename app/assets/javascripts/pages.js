aadd09d658526d4aea3084ffbf70471a8a4da00b
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-26 16:43:04 -0800

    Add modal for contact

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
 
+    //nav links
     $('.nameHeader').on('click', function() {
         $(this).parent().parent().find('.navLinks').slideToggle();
-    })
+    });
+
+
+    //modal
+    var modal = document.getElementById('contactInfo');
+    var span = document.getElementsByClassName("close")[0];
+    
+    $('#contactBtn').on('click', function() {
+        modal.style.display = "block";
+    });
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 
 });
-
-
 
 
 $(function() {
