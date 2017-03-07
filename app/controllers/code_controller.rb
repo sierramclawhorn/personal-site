5431cd7961cbc3089099d18a2449aa0f6dac0cca
sierramclawhorn <sierramclawhorn@gmail.com>
2017-03-07 11:52:36 -0800

    Update routes and code controller for partials

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -3,19 +3,4 @@ class CodeController < ApplicationController
 	def index
 	end
 
-	# def klink
-	# end
-
-	# def kathyandsierra
-	# end
-
-	# def kwantree
-	# end
-
-	# def clinicfinder
-	# end
-
-	# def americanparadise
-	# end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@ Rails.application.routes.draw do
 	resources :film
 	resources :code
 
-	# get 'klink', to: 'code#klink'
-	# get 'kathyandsierra', to: 'code#kathyandsierra'
-	# get 'kwantree', to: 'code#kwantree'
-	# get 'clinicfinder', to: 'code#clinicfinder'
-	# get 'americanparadise', to: 'code#americanparadise'
-
 	root 'pages#index'
 end
