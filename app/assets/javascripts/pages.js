3c57d4375c49f544a97460c95931966628f0706e
sierramclawhorn <sierramclawhorn@gmail.com>
2017-06-01 00:39:14 -0400

    Update code projects

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -86,7 +86,6 @@ $(document).on('turbolinks:load', function(){
 
 
 
-
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 $(document).ready(function($) {
     if(!isMobile) {
